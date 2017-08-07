5f181ce02248fb4dd578553867b668465ed133b4
Liu <xgeoo4@net.plm.eds.com>
2017-08-07 16:23:53 +0800

    update from workplace

--- Algorithms/Sort/SelectionSort.h
+++ Algorithms/Sort/SelectionSort.h
@@ -1,16 +1,18 @@
-#include <iostream>
+
 
 template<class RandomAccessIterator>
 void SelectionSort(RandomAccessIterator first, RandomAccessIterator last)
 {
     for (RandomAccessIterator i = first; i < last; ++i)
     {
+		// find min index
+		RandomAccessIterator min = i;
         for (RandomAccessIterator j = i + 1; j < last; ++j)
         {
-            if (i > j)
-                std::swap(i, j);
+			if (*min > *j)
+				min = j;	
         }
+
+		std::swap(*i, *min);
     }
-
 }
-
--- Algorithms/Sort/test.cpp
+++ Algorithms/Sort/test.cpp
@@ -8,10 +8,9 @@ int main()
     std::vector<int> testData;
     testData.push_back(5);
     testData.push_back(4);
+	testData.push_back(3);
     testData.push_back(2);
     testData.push_back(1);
-    testData.push_back(50);
-
 
     SelectionSort(testData.begin(), testData.end());
 
