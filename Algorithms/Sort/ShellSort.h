061caedb95abf438f6288b4ea264d723603cedd4
loureed927 <ltbuaa@gmail.com>
2017-08-12 00:41:52 +0800

    update ShellSort.h

--- Algorithms/Sort/ShellSort.h
+++ Algorithms/Sort/ShellSort.h
@@ -12,7 +12,8 @@ void ShellSort(RandomAccessIterator first, RandomAccessIterator last)
     // why use increment sequence like this?
     while (h < num / 3)
         h = 3 * h + 1; // 1, 4, 13,...
-
+    /*
+    // version 1
     while (h >= 1)
     {
         for (auto i = first; i + h != last; i++)
@@ -20,6 +21,41 @@ void ShellSort(RandomAccessIterator first, RandomAccessIterator last)
             if (*i > *(i + h))
             {
                 std::swap(*i, *(i + h));
+            }
+        }
+
+        // use next h down to 1 to perform sort.
+        h = (h - 1) / 3;
+    }
+    */
+
+    /*
+    // version 2
+    while (h >= 1)
+    {
+        for (auto i = first; i + h != last; i++)
+        {
+            // j-4 could out of iterator range
+            for (auto j = i + h; j != first && *j < *(j - h); j = j - h)
+            {
+                std::swap(*j, *(j - h));
+            }
+        }
+
+        // use next h down to 1 to perform sort.
+        h = (h - 1) / 3;
+    }
+    */
+
+    // version 3
+    while (h >= 1)
+    {
+        for (auto i = first; i + h != last; i++)
+        {
+            // note distance could return negative, put smaller iter front
+            for (auto j = i + h; std::distance(first, j) >= h && *j < *(j - h); j = j - h)
+            {
+                std::swap(*j, *(j - h));
             }
         }
 
