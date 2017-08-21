438f7a1080520394a97378531c20ddafefce6c5f
loureed927 <ltbuaa@gmail.com>
2017-08-22 01:41:41 +0800

    Update Partition method of QuickSort.

--- Algorithms/Sort/QuickSort.h
+++ Algorithms/Sort/QuickSort.h
@@ -4,6 +4,7 @@
 template<class T>
 int Partition(vector<T>& t, int lo, int hi)
 {
+/*   Attempt one(failed to handle equal keys)
     // make postion j point to lo's element.
     int j = lo;
 
@@ -29,11 +30,89 @@ int Partition(vector<T>& t, int lo, int hi)
     }
 
     // do the last exchange
+
     std::swap(t[j], t[--head]);
     j = head;
 
     // t[j] is now at its final postion.
+    return j
+*/
+
+/*   Attempt one(failed to handle max partition key)
+    // make postion j point to lo's element.
+    int j = lo;
+
+    int head = lo + 1;
+    int tail = hi;
+
+    while (head < tail)
+    {
+        // only increase head if its key smaller than partition key
+        if (t[head] < t[j])
+        {
+            head++;
+        }
+
+        // only decrease tail if its key larger than partition key
+        if (t[tail] > t[j])
+        {
+            tail--;
+        }
+
+        // keys equal to partition key also needs to be exchanged!!!(E C A I E)
+        if (t[head] >= t[j] && t[tail] <= t[j])
+        {
+            std::swap(t[head++], t[tail--]);
+        }
+    }
+
+    // do the last exchange
+    // need to consider partition key is larger than all left and right scan. (E C A)
+    std::swap(t[j], t[--head]);
+    j = head;
+
+    // t[j] is now at its final position.
     return j;
+*/
+
+    // make postion j point to lo's element.
+    int j = lo;
+
+    int head = lo + 1;
+    int tail = hi;
+
+    while (head <= tail)
+    {
+        // only increase head if its key smaller than partition key
+        if (t[head] < t[j] && t[tail] <= t[j])
+        {
+            head++;
+        }
+        // only decrease tail if its key larger than partition key
+        else if (t[head] >= t[j] && t[tail] > t[j])
+        {
+            tail--;
+        }
+        else if (t[head] < t[j] && t[tail] >t[j])
+        {
+            head++;
+            tail--;
+        }
+        // keys equal to partition key also needs to be exchanged!!!(E C A I E)
+        else if (t[head] >= t[j] && t[tail] <= t[j])
+        {
+            std::swap(t[head++], t[tail--]);
+        }
+    }
+
+    // do the last exchange
+    // need to consider partition key is larger than all left and right scan. (E C A)
+    std::swap(t[j], t[--head]);
+    j = head;
+
+    // t[j] is now at its final position.
+    return j;
+
 }
 
 template<class T>
@@ -42,8 +121,11 @@ void QuickSort(vector<T>& t, int lo, int hi)
     int j = Partition(t, lo, hi);
     while (j != lo)
     {
-        Partition(t, lo, j-1);
-        Partition(t, j+1, hi);
+        j = Partition(t, lo, j-1);
+    }
+    while (j != lo)
+    {
+        j = Partition(t, j + 1, hi);
     }
 }
 
