4a2d957f450162650ae4a69363d1814e43afcbf6
liutong927 <ltbuaa@gmail.com>
2017-08-24 18:20:33 +0800

    create Fundamentals Folder.

---
+++ Algorithms/Fundamentals/Stack.h
@@ -0,0 +1,41 @@
+//**************************************************************
+//              stack
+//**************************************************************
+
+// Fixed Capacity Stack
+template<typename T>
+class FixedCapacityStack
+{
+public:
+    FixedCapacityStack(int cap)
+    {
+        a = new T[cap];
+    };
+
+    ~FixedCapacityStack();
+
+    bool isEmpty()
+    {
+        return num == 0;
+    };
+
+    int size()
+    {
+        return num;
+    };
+
+    void push(T item)
+    {
+        
+    };
+
+    T pop()
+    {
+
+    };
+
+private:
+    T[] a;
+    int num;
+};
+
---
+++ Algorithms/Fundamentals/TestFundamentals.cpp
@@ -0,0 +1,12 @@
+//**************************************************************
+//          main test routine for test fundamental data structures
+//**************************************************************
+
+#include "Stack.h"
+
+
+int main()
+{
+    FixedCapacityStack<int> stack(5);
+}
+
