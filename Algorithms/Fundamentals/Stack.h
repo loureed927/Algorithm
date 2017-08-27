375a84a9a2474848430a685c435205b7382d7dea
loureed927 <ltbuaa@gmail.com>
2017-08-28 01:53:00 +0800

    Initial ResizingArrayStack.

--- Algorithms/Fundamentals/Stack.h
+++ Algorithms/Fundamentals/Stack.h
@@ -2,6 +2,9 @@
 //              stack
 //**************************************************************
 
+#ifndef STACK_H
+#define STACK_H
+
 // Fixed Capacity Stack
 template<typename T>
 class FixedCapacityStack
@@ -29,6 +32,7 @@ public:
 
     void push(T item)
     {
+        // push should check whether array is already full, omit here for simplicity.
         a[num++] = item;
     };
 
@@ -42,3 +46,71 @@ private:
     int num;
 };
 
+// Resizing Array Stack
+template<typename T>
+class ResizingArrayStack
+{
+public:
+    ResizingArrayStack()
+    {
+        a = new T[1];
+        num = 0;
+    };
+
+    ~ResizingArrayStack()
+    {
+        delete[] a;
+    };
+
+    bool isEmpty()
+    {
+        return num == 0;
+    };
+
+    int size()
+    {
+        return num;
+    };
+
+    void push(T item)
+    {
+        // if array is full, allocate double size.
+        if (a->size() == num)
+        {
+            resize(2 * num);
+        }
+
+        a[num++] = item;
+    };
+
+    T pop()
+    {
+        T t = a[--num];
+
+        if (num == a->size() / 4)
+        {
+            resize(a->size() / 2);
+        }
+
+        return t;
+    };
+
+private:
+
+    void resize(int max)
+    {
+        T* temp = new T[max];
+        for (int i = 0; i < num; i++)
+        {
+            temp[i] = a[i];
+        }
+
+        a = temp;
+    };
+
+    T* a;
+    int num;
+};
+
+
+#endif
--- Algorithms/Fundamentals/TestFundamentals.cpp
+++ Algorithms/Fundamentals/TestFundamentals.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 int main()
 {
-    FixedCapacityStack<string> stack(100);
+    //FixedCapacityStack<string> stack(100);
+    ResizingArrayStack<string> stack;
+
     ifstream inputFile("tobe.txt");
     string line, word;
 
