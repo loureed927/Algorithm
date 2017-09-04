383d5325db6d6e0c516d7144b3e2098d579e7e9f
loureed927 <ltbuaa@gmail.com>
2017-09-05 01:03:02 +0800

    Initial PriorityQueue.h

---
+++ Algorithms/Sort/PriorityQueue.h
@@ -0,0 +1,54 @@
+//**************************************************************
+//          priority queue
+//**************************************************************
+
+template<typename T>
+class MaxPQ
+{
+public:
+    MaxPQ(int max){}
+
+    ~MaxPQ(){}
+
+    void Insert(Key v)
+    {
+
+    }
+
+    Key Max()
+    {
+
+    }
+
+    Key DeleteMax()
+    {
+
+    }
+
+    bool IsEmpty()
+    {
+
+    }
+
+    int Size()
+    {
+
+    }
+
+private:
+
+    void swim(int k)
+    {
+
+    }
+
+    void sink(int k)
+    {
+
+    }
+
+    T[num] pq;
+    int num;
+};
+
+
