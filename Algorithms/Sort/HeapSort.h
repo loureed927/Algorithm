939c9ee0bb301afe0e2523a6ac3b3122e7783525
loureed927 <ltbuaa@gmail.com>
2017-09-07 01:34:30 +0800

    Update PriorityQueue.

---
+++ Algorithms/Sort/HeapSort.h
@@ -0,0 +1,14 @@
+//**************************************************************
+//          heap sort
+//**************************************************************
+
+template<class T>
+void HeapSort(vector<T>& t, int lo, int hi)
+{
+    // heap sort has two phases:
+    // 1. heap construction: reorganize the original array into a heap;
+    // 2. sortdown, pull the items out of the heap in decreasing order to build sorted result.
+
+
+}
+
--- Algorithms/Sort/PriorityQueue.h
+++ Algorithms/Sort/PriorityQueue.h
@@ -2,6 +2,7 @@
 //          priority queue
 //**************************************************************
 
+// binary heap based max priority queue.
 template<typename T>
 class MaxPQ
 {
@@ -31,7 +32,10 @@ public:
     {
         T max = pq[1];
         std::swap(pq[1], pq[N--]);
+
+        // now we have N-1 elements, pq[N] unused. since we decrease N already, make pq[N+1] null.
         pq[N + 1] = nullptr;
+
         sink(1);
         return max;
     }
@@ -61,21 +65,43 @@ private:
     void sink(int k)
     {
         // if parent is less than child, sink parent to child level.
-        while (k < N)
+        //while (k < N)
+        //{
+        //    if (pq[k]>=pq[2k] || pq[k]>=pq[2k+1])
+        //        break;
+
+        //    if (pq[k] < pq[2k])
+        //    {
+        //        std::swap(pq[k], pq[2k]);
+        //        k = 2k;
+        //    }
+        //    else if (pq[k] < pq[2k + 1])
+        //    {
+        //        std::swap(pq[k], pq[2k + 1]);
+        //        k = 2k + 1;
+        //    }
+        //}
+
+        while (2k <= N)
         {
-            if (pq[k]>=pq[2k] || pq[k]>=pq[2k+1])
+            // find the larger one of two children.
+            // this is to simplify the process of compare k and child.
+            // no need to compare with two children, we only compare k with larger one.
+            int child = 2k;
+            if (child < N && pq[child] < pq[child + 1])
+            {
+                child++;
+            }
+
+            // if element k is larger(equal) than the larger child, then no need to exchange,
+            // the k is at its position.
+            if (pq[k] >= pq[child])
                 break;
 
-            if (pq[k] < pq[2k])
-            {
-                std::swap(pq[k], pq[2k]);
-                k = 2k;
-            }
-            else if (pq[k] < pq[2k + 1])
-            {
-                std::swap(pq[k], pq[2k + 1]);
-                k = 2k + 1;
-            }
+            std::swap(pq[k], pq[child]);
+
+            k = child;
+
         }
     }
 
