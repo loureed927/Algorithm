5cdeeba078a8e8632ba08e355446b092da913a90
loureed927 <ltbuaa@gmail.com>
2017-11-06 01:45:47 +0800

    Initial DepthFirstSearch.h.

--- Algorithms/4-Graph/DepthFirstSearch.h
+++ Algorithms/4-Graph/DepthFirstSearch.h
@@ -5,12 +5,63 @@
 #ifndef DEPTHFIRSTSEARCH_H
 #define DEPTHFIRSTSEARCH_H
 
+#include "Graph.h"
+
 class DepthFirstSearch
 {
 public:
+    DepthFirstSearch(Graph g, int s)
+    {
+        int vertices = g.Vertices();
+        marked = new bool[vertices];
+        for (int m = 0; m < vertices; m++)
+        {
+            marked[m] = false;
+        }
+
+        dfs(g, s);
+    }
+
+    ~DepthFirstSearch()
+    {
+        delete[] marked;
+    }
+
+    // is w connected to s?
+    bool Marked(int w)
+    {
+        return marked[w];
+    }
+
+    // how many vertices are connected to s?
+    int Count()
+    {
+        return count;
+    }
 
 private:
+    void dfs(Graph g, int v)
+    {
+        // to visit a vertex, marked it as having been visited.
+        marked[v] = true;
+        for (int a : g.AdjacentToVertex(v))
+        {
+            // if adjacent vertex of v is not visited, continue with dfs.
+            if (!marked[a])
+            {
+                // adjacent unvisited vertex count for connected vertices.
+                count++;
+                dfs(g, a);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
 
+    bool* marked; // array of bool values to mark all of the vertices that are connected to the source.
+    int count;
 };
 
 #endif
--- Algorithms/4-Graph/TestGraph.cpp
+++ Algorithms/4-Graph/TestGraph.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include "Graph.h"
+#include "DepthFirstSearch.h"
 
 using namespace std;
 
@@ -15,8 +16,34 @@ void GraphConstruct_TestClient()
     cout << g.ToString() << endl;
 }
 
+void GraphDFS_TestClient(int source)
+{
+    ifstream inputFile("tinyG.txt");
+    Graph g(inputFile);
+    DepthFirstSearch dfs(g, source);
+
+    // loop all vertices, print vertices connected to source.
+    for (int v = 0; v < g.Vertices(); v++)
+    {
+        if (dfs.Marked(v))
+        {
+            cout << v << " " << endl;
+        }
+    }
+
+    // print whether the graph is connected.
+    // the graph is connected if and only if the search marked all of its vertices.
+    if (dfs.Count() != g.Vertices())
+    {
+        cout << "NOT";
+    }
+    cout << "connected" << endl;
+}
+
 int main()
 {
     GraphConstruct_TestClient();
+    GraphDFS_TestClient(0);
+    GraphDFS_TestClient(9);
 }
 
