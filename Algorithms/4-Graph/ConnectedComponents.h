c93c4869c688e7adea82aca8dbd440406682ee6f
liutong927 <ltbuaa@gmail.com>
2017-11-15 17:27:47 +0800

    Initial ConnectedComponents.h.

---
+++ Algorithms/4-Graph/ConnectedComponents.h
@@ -0,0 +1,45 @@
+//**************************************************************
+//                  Connected Components
+//**************************************************************
+
+#ifndef CONNECTEDCOMPONENTS_H
+#define CONNECTEDCOMPONENTS_H
+
+#include "Graph.h"
+
+class ConnectedComponents
+{
+public:
+    ConnectedComponents(Graph& g)
+    {
+
+    }
+
+    ~ConnectedComponents()
+    {
+
+    }
+
+    // are vertex v and w connected?
+    bool Connected(int v, int w)
+    {
+
+    }
+
+    // number of connected components.
+    int Count()
+    {
+
+    }
+
+    // components identifier for vertex v.(between 0 and Count()-1)
+    int Id(int v)
+    {
+
+    }
+
+private:
+
+};
+
+#endif
