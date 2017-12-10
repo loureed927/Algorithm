407615fe9af4348e3b241e106d2656798245c638
loureed927 <ltbuaa@gmail.com>
2017-12-10 21:45:49 +0800

    Add Edge.h and EdgeWeightedGraph.h.

---
+++ Algorithms/4-Graph/Edge.h
@@ -0,0 +1,87 @@
+//**************************************************************
+//                  Edge
+//**************************************************************
+
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <string>
+#include <boost/format.hpp>
+
+using namespace std;
+
+// weighted edge
+class Edge
+{
+public:
+    Edge(int v, int w, double weight)
+    {
+        this->v = v;
+        this->w = w;
+        this->weight = weight;
+    }
+
+    ~Edge()
+    {
+    }
+
+    bool operator< (Edge& e)
+    {
+        return this->weight < e.weight;
+    }
+
+    bool operator> (Edge& e)
+    {
+        return this->weight > e.weight;
+    }
+
+    bool operator== (Edge& e)
+    {
+        return this->weight == e.weight;
+    }
+
+    bool operator!= (Edge& e)
+    {
+        return this->weight != e.weight;
+    }
+
+    double Weight()
+    {
+        return weight;
+    }
+
+    // either of this edge's vertices.
+    int Either()
+    {
+        return v;
+    }
+
+    // the other vertex.
+    int Other(int vertex)
+    {
+        if (vertex == v)
+        {
+            return w;
+        }
+        else if (vertex == w)
+        {
+            return v;
+        }
+        else
+        {
+            throw(vertex);
+        }
+    }
+
+    string ToString()
+    {
+        return str(boost::format("%d - %d %.2f") % v %w %weight);
+    }
+
+private:
+    int v;
+    int w;
+    double weight;
+};
+
+#endif
